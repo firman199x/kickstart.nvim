b47f2ccfd77181ef59aa90db77f6adf677231d75
Firman Syah Sukri <firmans@willow.asia>
2026-06-23 16:06:53 +0800

    set nowrap

--- init.lua
+++ init.lua
@@ -32,6 +32,8 @@ do
 	vim.o.scrolloff = 10
 	vim.o.confirm = true
 
+	vim.o.wrap = false
+
   vim.opt.tabstop = 4      -- Number of spaces that a <Tab> in the file counts for
   vim.opt.shiftwidth = 4   -- Size of an indent
   vim.opt.softtabstop = 4  -- Number of spaces that a <Tab> counts for while performing editing operations
